9ca3df982670fce9a40303b7093bbe31a97b4345
Ismail <makhmudbekovismail@gmail.com>
2023-05-03 21:56:30 +0500

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+"""
+{% url 'main:home' %}
+{% url 'pages:home' %}
+"""
+app_name = "main"
+
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path("shop/", views.shop_view, name="shop"),
+    path("shop/categories/<slug:slug>/", views.category_products, name="category_products")
+]
